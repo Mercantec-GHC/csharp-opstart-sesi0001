5e50b45851a0e4ac0000843c08da77c834fa1927
sesi0001 <146714747+sesi0001@users.noreply.github.com>
2024-02-22 22:45:21 +0100

    update

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -237,14 +237,12 @@ for (int i = (pangramSplit2.Length - 1); i >= 0; i--)
     Console.Write(finalString);
 }
 
+
 Console.WriteLine("");
 
 string orderStream = "B123,C234,A345,C15,B177,G3003,C235,B179";
 string[] orderStreamArray = orderStream.Split(",");
 Array.Sort(orderStreamArray);
-
-
-Console.WriteLine(orderStreamArray);
 
 foreach (var order in orderStreamArray)
 {
@@ -259,4 +257,98 @@ foreach (var order in orderStreamArray)
     }
 }
 
+
+
+string first4 = "Hello";
+string second4 = "World";
+string result4 = string.Format("{0} {1}!", first4, second4);
+Console.WriteLine(result4);
+
+Console.WriteLine("{1} {0}!", first4, second4);
+Console.WriteLine("{0} {0} {0}!", first4, second4);
+
+Console.WriteLine($"{first4} {second4}!");
+Console.WriteLine($"{second4} {first4}!");
+Console.WriteLine($"{first4} {first4} {first4}!");
+
+decimal price = 123.45m;
+int discount = 50;
+Console.WriteLine($"Price: {price:C} (Save {discount:C})");
+
+decimal measurement = 123456.78912m;
+Console.WriteLine($"Measurement: {measurement:N5} units");
+
+decimal tax = .36785m;
+Console.WriteLine($"Tax rate: {tax:P2}");
+
+
+decimal price3 = 67.55m;
+decimal salePrice = 59.99m;
+
+string yourDiscount = String.Format("Your discount is: {0:C2}. of the original price: {1:C2}.", (price3 - salePrice), price3);
+yourDiscount += $" A discount of {(price3 - salePrice) / price3:P2}!";
+
+Console.WriteLine(yourDiscount);
+
+
+
+int invoiceNumber = 1201;
+decimal productShares = 25.4568m;
+decimal subtotal = 2750.00m;
+decimal taxPercentage = .15825m;
+decimal total = 3185.19m;
+
+Console.WriteLine($"Invoice Number: {invoiceNumber}");
+Console.WriteLine($"    Shares: {productShares:N3} product");
+Console.WriteLine($"     Sub Total: {subtotal:C}");
+Console.WriteLine($"           Tax: {taxPercentage:P2}");
+Console.WriteLine($"     Total Billed: {total:C}");
+
+
+string input = "Pad this";
+Console.WriteLine(input.PadRight(12, '-'));
+
+
+string paymentId = "769C";
+string payeeName = "Mr. Stephen Ortega";
+string paymentAmount = "$5,000.00";
+
+var formattedLine = paymentId.PadRight(6);
+formattedLine += payeeName.PadRight(24);
+formattedLine += paymentAmount.PadLeft(10);
+
+Console.WriteLine("1234567890123456789012345678901234567890");
+Console.WriteLine(formattedLine);
+
+
+Console.WriteLine("");
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+// Your logic here
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.");
+Console.WriteLine("");
+Console.WriteLine($"Currently, you own {currentShares} shares at a return of {currentReturn:P2}.");
+Console.WriteLine("");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P2}.  Given your current volume, your potential profit would be {newProfit:C}");
+Console.WriteLine("");
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = $"{currentProduct}         {currentReturn:P2}   {currentProfit:C}";
+string comparisonMessage2 = $"{newProduct}     {newReturn:P2}   {newProfit:C}";
+
+// Your logic here
+
+Console.WriteLine(comparisonMessage);
+Console.WriteLine(comparisonMessage2);
 
