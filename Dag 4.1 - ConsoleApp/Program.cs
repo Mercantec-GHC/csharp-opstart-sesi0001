ba3aa086cbe4a2112fa7feeb28573fe14fb76015
sesi0001 <146714747+sesi0001@users.noreply.github.com>
2024-02-23 09:32:31 +0100

    update

--- Dag 4.1 - ConsoleApp/Program.cs
+++ Dag 4.1 - ConsoleApp/Program.cs
@@ -352,3 +352,170 @@ string comparisonMessage2 = $"{newProduct}     {newReturn:P2}   {newProfit:C}";
 Console.WriteLine(comparisonMessage);
 Console.WriteLine(comparisonMessage2);
 
+/*
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
+*/
+Console.WriteLine("");
+
+/*
+string message = "Find what is (inside the parentheses)";
+
+int openingPosition = message.IndexOf('(');
+int closingPosition = message.IndexOf(')');
+
+//Console.WriteLine(openingPosition);
+//Console.WriteLine(closingPosition);
+
+openingPosition += 1;
+
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
+/*
+string message = "What is the value <span>between the tags</span>?";
+
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition = message.IndexOf(openSpan);
+int closingPosition = message.IndexOf(closeSpan);
+
+openingPosition += openSpan.Length;
+int length = closingPosition - openingPosition;
+Console.WriteLine(message.Substring(openingPosition, length));
+*/
+
+/*
+string message = "(What if) I am (only interested) in the last (set of parentheses)?";
+
+while (true)
+{
+    int openingPosition = message.IndexOf('(');
+    if (openingPosition == -1)
+    {
+        break;
+    }
+
+    openingPosition += 1;
+    int closingPosition = message.IndexOf(')');
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+
+    message = message.Substring(closingPosition + 1);
+}
+*/
+
+/*
+string message = "Help (find) the {opening symbols}";
+Console.WriteLine($"Searching THIS Message: {message}");
+char[] openSymbols = { '[', '{', '(' };
+int startPosition = 5;
+int openingPosition = message.IndexOfAny(openSymbols);
+Console.WriteLine($"Found WITHOUT using startPosition: {message.Substring(openingPosition)}");
+
+openingPosition = message.IndexOfAny(openSymbols, startPosition);
+Console.WriteLine($"Found WITH using startPosition {startPosition}:  {message.Substring(openingPosition)}");
+*/
+
+
+
+string message = "(What if) I have [different symbols] but every {open symbol} needs a [matching closing symbol]?";
+
+// The IndexOfAny() helper method requires a char array of characters. 
+// You want to look for:
+
+char[] openSymbols = { '[', '{', '(' };
+
+// You'll use a slightly different technique for iterating through 
+// the characters in the string. This time, use the closing 
+// position of the previous iteration as the starting index for the 
+//next open symbol. So, you need to initialize the closingPosition 
+// variable to zero:
+
+int closingPosition = 0;
+
+while (true)
+{
+    int openingPosition = message.IndexOfAny(openSymbols, closingPosition);
+
+    if (openingPosition == -1) break;
+
+    string currentSymbol = message.Substring(openingPosition, 1);
+
+    // Now  find the matching closing symbol
+    char matchingSymbol = ' ';
+
+    switch (currentSymbol)
+    {
+        case "[":
+            matchingSymbol = ']';
+            break;
+        case "{":
+            matchingSymbol = '}';
+            break;
+        case "(":
+            matchingSymbol = ')';
+            break;
+    }
+
+    // To find the closingPosition, use an overload of the IndexOf method to specify 
+    // that the search for the matchingSymbol should start at the openingPosition in the string. 
+
+    openingPosition += 1;
+    closingPosition = message.IndexOf(matchingSymbol, openingPosition);
+
+    // Finally, use the techniques you've already learned to display the sub-string:
+
+    int length = closingPosition - openingPosition;
+    Console.WriteLine(message.Substring(openingPosition, length));
+}
+
+
+string data2 = "12345John Smith          5000  3  ";
+string updatedData = data2.Remove(5, 20);
+Console.WriteLine(updatedData);
+
+
+string message2 = "This--is--ex-amp-le--da-ta";
+message2 = message2.Replace("--", " ");
+message2 = message2.Replace("-", "");
+Console.WriteLine(message2);
+
+
+const string input2 = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
+
+string quantity = "";
+string output = "";
+
+// Your work here
+const string openSpan = "<span>";
+const string closeSpan = "</span>";
+
+int openingPosition2 = input2.IndexOf(openSpan);
+int closingPosition2 = input2.IndexOf(closeSpan);
+
+openingPosition2 += openSpan.Length;
+int length2 = closingPosition2 - openingPosition2;
+quantity = input2.Substring(openingPosition2, length2);
+
+output = input2.Replace("<div>", "");
+output = output.Replace("</div>", "");
+output = output.Replace("trade", " reg");
+
+Console.WriteLine(quantity);
+Console.WriteLine(output);
+
+//Quantity: 5000
+//Output: < h2 > Widgets & reg;</ h2 >< span > 5000 </ span >
